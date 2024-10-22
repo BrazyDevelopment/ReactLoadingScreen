c1590c69cd9585c28ebe94404018441e6cca6e23
BrazyDevelopment <lee@lsments.com>
2024-10-22 08:56:24 +0100

    config

--- fivem-loading-screen/vite.config.ts
+++ fivem-loading-screen/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react()],
   base: './',
   resolve: {
     alias: {
@@ -13,24 +12,12 @@ export default defineConfig({
   },
   build: {
     outDir: '../tmf-loadingscreen/web/dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    },
-    sourcemap: false,
+    sourcemap: true,
     emptyOutDir: true,
   },
   css: {
     modules: {
       generateScopedName: '[name]__[local]___[hash:base64:5]',
     },
-  },
-  // Server configuration if needed
-  server: {
-    port: 3000,
   },
 });
