0241bd4dc4f56e8bd49a4f6e9dde8c67ff5ea9b7
BrazyDevelopment <lee@lsments.com>
2024-10-22 08:24:17 +0100

    fivem fix?

--- tmf-loadingscreen/fxmanifest.lua
+++ tmf-loadingscreen/fxmanifest.lua
@@ -4,15 +4,15 @@ game 'gta5'
 description 'Custom Loading Screen'
 version '1.0.0'
 
-loadscreen 'web/index.html'
-ui_page 'web/index.html'
+loadscreen 'web/dist/index.html'
+ui_page 'web/dist/index.html'
 loadscreen_cursor 'yes'
 loadscreen_manual_shutdown 'yes'
 
 files {
-    'web/index.html',
-    'web/dist/assets/*',
-    'web/dist/*'
+    'web/dist/index.html',
+    'web/dist/assets/**.*',
+    'web/dist/**.*'
 }
 
 client_script 'client.lua'
